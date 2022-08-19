20eb7265752b114e13948381f29442658ddad724
DAndyPhm <s3818286@rmit.edu.vn>
2022-08-19 12:25:12 +0700

    Assignment started

---
+++ s3818286_Assign2/Views/MenuView.swift
@@ -0,0 +1,20 @@
+//
+//  MenuView.swift
+//  s3818286_Assign2
+//
+//  Created by An Pham Hoang Thien on 19/08/2022.
+//
+
+import SwiftUI
+
+struct MenuView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MenuView_Previews: PreviewProvider {
+    static var previews: some View {
+        MenuView()
+    }
+}
