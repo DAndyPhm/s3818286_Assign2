28d3a2cb0d6ebbd0962ecb93ac26c9f4ade9d031
DAndyPhm <s3818286@rmit.edu.vn>
2022-08-19 11:41:28 +0700

    Initial Commit

---
+++ s3818286_Assign2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  s3818286_Assign2
+//
+//  Created by An Pham Hoang Thien on 19/08/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ s3818286_Assign2/s3818286_Assign2App.swift
@@ -0,0 +1,17 @@
+//
+//  s3818286_Assign2App.swift
+//  s3818286_Assign2
+//
+//  Created by An Pham Hoang Thien on 19/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct s3818286_Assign2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
